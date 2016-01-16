782c76860af74745918b5268bbac54cd7b39dfc5
jeffslutzky <jeffslutzky@gmail.com>
2016-01-16 15:04:11 -0500

    finished

--- js/aplus.js
+++ js/aplus.js
@@ -1,8 +1,10 @@
 $(function(){
     $.getJSON("http://aplus.com/api/v1/sidebar_aplus_data.json?callback=?", function(data){
+      var sponsored = data.trending.shift();
+      sponsored.sponsor = "TGIFridays";
+      $(".container").append("<div class='sponsored'><div class='smcaps'>Sponsored by " + sponsored.sponsor +"</div><p><a target='_blank' href='" + sponsored.url + "'>" + sponsored.title + "</a></p><img width='400' src='" + sponsored.thumbnail + "'</></div>")
       $.each(data.trending, function(index, item){
-        $("ul").append("<li><a target='_blank' href='" + item.url + "'>" + item.title + "</a></li>")
-        $("ul").append("<img width='400' src='" + item.thumbnail + "'</>")
+        $(".container").append("<p><a target='_blank' href='" + item.url + "'>" + item.title + "</a></p><img width='400' src='" + item.thumbnail + "'>")
       })
     });
 })
