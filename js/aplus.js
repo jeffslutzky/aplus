4b88814e7268d648818b83642ac7ba8b096e8b2f
jeffslutzky <jeffslutzky@gmail.com>
2016-01-15 23:14:27 -0500

    create basic presentation

--- js/aplus.js
+++ js/aplus.js
@@ -1,6 +1,8 @@
 $(function(){
-  $.getJSON("http://aplus.com/api/v1/sidebar_aplus_data.json?callback=?", function(data){
-    debugger;
-  });
-
+    $.getJSON("http://aplus.com/api/v1/sidebar_aplus_data.json?callback=?", function(data){
+      $.each(data.trending, function(index, item){
+        $("ul").append("<li><a target='_blank' href='" + item.url + "'>" + item.title + "</a></li>")
+        $("ul").append("<img width='400' src='" + item.thumbnail + "'</>")
+      })
+    });
 })
